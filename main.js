e3ddc5f24b06cc8dca3c7bbe78837051e10ce78e
Amod Manjarekar <amodm.1102@gmail.com>
2024-05-31 23:29:32 +0530

    Add comments

--- main.js
+++ main.js
@@ -4,21 +4,23 @@
 // 3. If the opponent says YES twice consecutively, always respond YES
 // 4. Repeat until all rounds end
 
+// importing required libraries
 const fs = require("node:fs");
 
-var my_res;
+var my_res; // to store final response for this round
 
-const round_no = parseInt(process.argv[2]);
-const prev_res = process.argv[3]?.toString();
+const round_no = parseInt(process.argv[2]); // to store current round number (not needed for this strategy)
+const prev_res = process.argv[3]?.toString(); // to store opponent's response in the previous round
 
+// we shall store one past response & our current response in a file
 fs.readFile("./folder1/mytext", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  past_res = data.split("\n")[0];
-  my_res = data.split("\n")[1];
+  past_res = data.split("\n")[0]; // reading opponent response in the round before previous round
+  my_res = data.split("\n")[1]; // reading our response in the last round
 
   if (prev_res == past_res) {
     if (prev_res == "YES") {
@@ -28,6 +30,7 @@ fs.readFile("./folder1/mytext", "utf8", (err, data) => {
     }
   }
 
+  // writing new responses to file
   fs.writeFile("./folder1/mytext", `${prev_res}\n${my_res}`, () => {
     if (err) {
       console.log(err);
